ec4c584dbe31e447fe5c90d359188f7443be4056
161250125 <161250125@mail.smal.nju.edu.cn>
2017-06-13 19:50:05 +0800

    Imp 好了

--- src/serviceImpl/IOServiceImpl.java
+++ src/serviceImpl/IOServiceImpl.java
@@ -1,6 +1,9 @@
 package serviceImpl;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.rmi.RemoteException;
@@ -14,10 +17,11 @@ public class IOServiceImpl extends UnicastRemoteObject implements IOService{
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
+	String prefix1="E:\\学习\\大作业\\BFServer\\";
+	String prefix2=".txt";
 	@Override
-	public boolean writeFile(String file, String userId, String fileName) {
-		File f = new File(userId + "_" + fileName);
+	public boolean writeFile(String file, String userId, String fileName) {//修改文件
+		File f = new File(prefix1+userId + "_" + fileName+prefix2);
 		try {
 			FileWriter fw = new FileWriter(f, false);
 			fw.write(file);
@@ -33,13 +37,48 @@ public class IOServiceImpl extends UnicastRemoteObject implements IOService{
 	@Override
 	public String readFile(String userId, String fileName) {
 		// TODO Auto-generated method stub
-		return "OK";
+		String result="";
+		try {
+			File myfile=new File(prefix1+userId + "_" + fileName+prefix2);
+			FileReader fileReader= new FileReader(myfile);
+			BufferedReader buffer=new BufferedReader(fileReader);
+			String Line=null;
+			while((Line=buffer.readLine())!=null){
+				result+=Line;
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 	@Override
 	public String readFileList(String userId) {
 		// TODO Auto-generated method stub
-		return "OK";
+		String result="";
+		File myfile=new File(prefix1+userId+prefix2);
+		try {
+			FileReader fileReader=new FileReader(myfile);
+			BufferedReader buffer=new BufferedReader(fileReader);
+			String Line=null;
+			try {
+				while((Line=buffer.readLine())!=null){
+					result+=Line+"\n";
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return result;
 	}
 	
 }
